69555612528dbe1fb58c8d29980887b9e56126a0
Dan Guldbransen <84964427+dan-guldbransen@users.noreply.github.com>
2023-05-19 11:19:39 +0200

    changed back header name

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/headers';
+import Header from './components/header';
 import Sliders from './components/sliders';
 import ApplyButton from './components/applybutton';
 
