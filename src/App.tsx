a04a3042ddb1307d3f9aa4fed72f15f0b54d47e8
Dan Guldbransen <84964427+dan-guldbransen@users.noreply.github.com>
2023-05-19 11:18:52 +0200

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/header';
+import Header from './components/headers';
 import Sliders from './components/sliders';
 import ApplyButton from './components/applybutton';
 
